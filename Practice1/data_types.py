1b8b7cb7364bcd625d0c6e5c6c1e190066afcf70
bekriri <sembaevbek04@gmail.com>
2026-06-18 14:36:53 +0500

    Create data_types.py

---
+++ Practice1/data_types.py
@@ -0,0 +1,33 @@
+# data types in python
+
+# int
+x = 10
+print(type(x))
+
+# float
+y = 3.14
+print(type(y))
+
+# string
+name = "Bek"
+print(type(name))
+
+# boolean
+is_student = True
+print(type(is_student))
+
+# list
+fruits = ["apple", "banana", "cherry"]
+print(type(fruits))
+
+# tuple
+coords = (10, 20)
+print(type(coords))
+
+# dictionary
+person = {"name": "Bekri", "age": 21}
+print(type(person))
+
+# set
+nums = {1, 2, 3, 4}
+print(type(nums))
