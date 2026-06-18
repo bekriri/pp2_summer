a00732f5ada74feb485975c04ae97cb23dd9c7ca
bekriri <sembaevbek04@gmail.com>
2026-06-18 13:40:25 +0500

    Create lambda.py

---
+++ Practice3/functions/lambda.py
@@ -0,0 +1,14 @@
+# lambda basics
+
+double = lambda x: x * 2
+print(double(5))
+
+add = lambda a, b: a + b
+print(add(3, 4))
+
+is_positive = lambda x: True if x > 0 else False
+print(is_positive(10))
+print(is_positive(-3))
+
+square = lambda x: x ** 2
+print(square(6))
