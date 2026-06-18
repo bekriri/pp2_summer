10564ab48131a3076b0df430a63de43f5310e78f
bekriri <sembaevbek04@gmail.com>
2026-06-18 14:35:46 +0500

    Create variables.py

---
+++ Practice1/variables.py
@@ -0,0 +1,28 @@
+# variables in python
+
+name = "Bekri"
+age = 21
+city = "Astana"
+
+print(name)
+print(age)
+print(city)
+
+age = 21
+print(age)
+
+# multiple assignment
+x, y, z = 1, 2, 3
+print(x, y, z)
+
+# all same value
+a = b = c = 10
+print(a, b, c)
+
+# global variable
+country = "Kazakhstan"
+
+def show():
+    print(country)
+
+show()
